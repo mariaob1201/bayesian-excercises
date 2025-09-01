1d7081ce218fbb5b4ce3f579b052067edff232e6
Maria Oros <maria.oros1201@gmail.com>
2025-09-01 17:23:30 -0500

    Refine plots for manuscript

--- Chapter3/predictive.R
+++ Chapter3/predictive.R
@@ -1,6 +1,44 @@
 ### Please visit the resources of JAGS, an introduction
 # https://d3c33hcgiwev3.cloudfront.net/_4ec003e4706226af504d524aafb2c527_JAGSintro.html?Expires=1756771200&Signature=OdYkCeDjRpfgsmb-hrB~ae7gjZTlq5-eqo8M39pt9B9BTSlGxfyPzCygkRfd5-dbe0T5O3ZF-4ZKRujTtrjndGgpOQr-fSpeamvQ49JhdKP4h7F-jVBInqsGL2ARoNjAyQQYVZvKW82Rw98Q2fOrS3ksBWLqVaN~DzSOI-pAQrg_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A
 
+library("rjags")
+
+mod_string = " model {
+  for (i in 1:n) {
+    y[i] ~ dnorm(mu, 1.0/sig2)
+  }
+  mu ~ dt(0.0, 1.0/1.0, 1.0) # location, inverse scale, degrees of freedom
+  sig2 = 1.0
+} "
+
+## setting up the model
+set.seed(50)
+y = c(1.2, 1.4, -0.5, 0.3, 0.9, 2.3, 1.0, 0.1, 1.3, 1.9)
+n = length(y)
+
+data_jags = list(y=y, n=n)
+params = c("mu")
+
+inits = function() {
+  inits = list("mu"=0.0)
+} # optional (and fixed)
+
+mod = jags.model(textConnection(mod_string), data=data_jags, inits=inits)
+
+## run mcmc sampler
+update(mod, 500) # burn-in
+
+mod_sim = coda.samples(model=mod,
+                       variable.names=params,
+                       n.iter=1000)
+
+## post processing
+summary(mod_sim)
+library("coda")
+plot(mod_sim)
+
+
+############################### Example from callers
 library(readr); library(coda); library(rjags)
 
 dat <- read.csv("callers.csv")
